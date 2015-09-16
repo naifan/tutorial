3982476449fcec449e063215c10a5b4fd424a4ab
naifan <haokqgood@gmail.com>
2015-09-16 15:01:51 +0800

    using extact( ) and dict()

--- tutorial/items.py
+++ tutorial/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class DmozlItem(scrapy.Item):
+class DmozItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
